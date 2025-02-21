86e1d432d5f57822bcdf4f4d4b3ed1f939c6b1e5
vinitj088 <vinitjain088@gmail.com>
2025-02-21 11:37:26 +0000

    Fix: Configure asset pipeline with correct manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,3 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../builds
-//= link controllers/application.js
-//= link controllers/note_modal_controller.js
-//= link controllers/debug_controller.js
-//= link_tree ../../../vendor/javascript .js
+//= link application.css
+//= link application.js
